389ad771a89da7f1be3bd180381724b72ae57b81
Your Name <leobarrientos02@gmail.com>
2022-03-14 15:48:00 -0400

    Search through API

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import { BrowserRouter } from "react-router-dom";
+import Search from "./components/Search";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Search />
       <Category />
       <Pages />
     </BrowserRouter>
